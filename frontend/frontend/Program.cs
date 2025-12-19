2a3ef30c4d74cc03ece8c9990517f8652eb7eeed
Sanjay Verma <sj@vmi2975006.contaboserver.net>
2025-12-19 20:09:00 +0100

    OIDC

--- frontend/frontend/Program.cs
+++ frontend/frontend/Program.cs
@@ -1,19 +1,25 @@
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using frontend;
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-namespace frontend
+builder.RootComponents.Add<App>("#app");
+
+// OIDC (Keycloak)
+builder.Services.AddOidcAuthentication(options =>
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-            builder.RootComponents.Add<HeadOutlet>("head::after");
+    options.ProviderOptions.Authority =
+        "https://auth.blackhatbadshah.com/realms/blackhatbadshah";
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+    options.ProviderOptions.ClientId = "blackhatbadshah-spa";
+    options.ProviderOptions.ResponseType = "code";
+    options.ProviderOptions.DefaultScopes.Clear();
+    options.ProviderOptions.DefaultScopes.Add("openid");
+    options.ProviderOptions.DefaultScopes.Add("profile");
+    options.ProviderOptions.DefaultScopes.Add("email");
+});
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped(_ =>
+    new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();

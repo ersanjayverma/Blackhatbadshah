c6b3e38748dee0d3015e95dc52c4aa9adb8cea86
Sanjay Verma <sj@vmi2975006.contaboserver.net>
2025-12-22 13:37:01 +0100

    Offline access added

--- frontend/frontend/Program.cs
+++ frontend/frontend/Program.cs
@@ -1,26 +1,34 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.Authorization;
 using frontend;
+using Blazored.LocalStorage;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<App>("#app");
 
-// OIDC (Keycloak)
+// 1. Consolidated OIDC Configuration
 builder.Services.AddOidcAuthentication(options =>
 {
-    options.ProviderOptions.Authority =
-        "https://auth.blackhatbadshah.com/realms/blackhatbadshah";
-
+    // Hardcoded overrides
+    options.ProviderOptions.Authority = "https://auth.blackhatbadshah.com/realms/blackhatbadshah";
     options.ProviderOptions.ClientId = "blackhatbadshah-spa";
     options.ProviderOptions.ResponseType = "code";
+    options.ProviderOptions.DefaultScopes.Add("offline_access"); // Request refresh tokens
     options.ProviderOptions.DefaultScopes.Clear();
     options.ProviderOptions.DefaultScopes.Add("openid");
     options.ProviderOptions.DefaultScopes.Add("profile");
     options.ProviderOptions.DefaultScopes.Add("email");
 });
-builder.Services.AddAuthorizationCore();
-builder.Services.AddScoped(_ =>
-    new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-    });
-    
-await builder.Build().RunAsync();
+
+
+builder.Services.AddBlazoredLocalStorage();
+
+
+builder.Services.AddScoped(sp => new HttpClient 
+{ 
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
+});
+
+await builder.Build().RunAsync(); // Use RunAsync() for WASM
